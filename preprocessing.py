2e2dbe14fa9609e0c6cd16f5901f2e3f038959b9
Shivank Tripathi <st.is.champion@gmail.com>
2026-03-20 13:04:37 +0530

    completed preprocessing code

---
+++ preprocessing.py
@@ -0,0 +1,26 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+def setup_nltk():
+    try:
+        nltk.data.find('corpora/stopwords')
+    except LookupError:
+        nltk.download('stopwords')
+
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+
+setup_nltk()
+stop_words = set(stopwords.words("english"))
+ps = PorterStemmer()
+
+def preprocess(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    words = text.split()
+    words = [ps.stem(w) for w in words if w not in stop_words]
+    return " ".join(words)
